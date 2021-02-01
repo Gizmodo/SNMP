1bb7cab0c0f3188d19aed03f87d4c86e353f3417
Sergey <smirnovsergeyterem@gmail.com>
2021-02-02 01:46:41 +0300

    RxCpp and SNMPManager added

---
+++ SnmpManager/Host.h
@@ -0,0 +1,29 @@
+//
+// Created by user on 02.02.2021.
+//
+
+#ifndef SNMPMANAGER_HOST_H
+#define SNMPMANAGER_HOST_H
+
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <string>
+
+struct cc_oid {
+    oid o[MAX_OID_LEN];
+    size_t length;
+};
+
+class Host {
+public:
+    std::string hostName;       //NICKNAME
+    std::string ip;             //IP
+    struct snmp_session *pSession;
+    std::vector<cc_oid> listOid;
+
+    Host() {}
+
+    ~Host() {}
+};
+
+#endif //SNMPMANAGER_HOST_H
---
+++ SnmpManager/Manager.h
@@ -0,0 +1,48 @@
+//
+// Created by user on 02.02.2021.
+//
+
+#ifndef SNMPMANAGER_MANAGER_H
+#define SNMPMANAGER_MANAGER_H
+
+#include <vector>
+#include "Host.h"
+#include <functional>
+
+class Manager {
+public:
+    Manager();
+
+    ~Manager();
+
+    void set_func(std::function<bool(Host, snmp_pdu *)> f);
+
+    void run();
+
+    void stop();
+
+    void add_host(Host *h);
+
+    void handle_data(Host *h, snmp_pdu *p);
+
+    void set_interval(uint32_t i);
+
+    //bool add_host_info(std::vector<char*> hosts, std::vector<char*> oids);
+
+private:
+    void init_sessions();
+
+    void asyn_send();
+
+    void wait_request();
+
+private:
+    std::vector<Host *> m_hosts;
+    std::function<bool(Host, snmp_pdu *)> m_handleFunc;
+    bool m_running;
+    uint32_t m_sendCount;
+    uint32_t m_loopInterval;
+};
+
+
+#endif //SNMPMANAGER_MANAGER_H
